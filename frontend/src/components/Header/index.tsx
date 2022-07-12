ca32d6b04d59e55de1b8571ae8333713ca044141
Sutanrrier <pabloryanbr0@gmail.com>
2022-07-12 15:52:56 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from "../../images/logo.svg";
+import "./styles.css";
+
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://github.com/Sutanrrier"> @Sutanrrier</a>
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
