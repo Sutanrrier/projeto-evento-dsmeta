afd36802c6119116545913240a697ea3bacc5908
Sutanrrier <pabloryanbr0@gmail.com>
2022-07-13 13:48:46 -0300

    API test

---
+++ backend/src/main/java/com/sutanrrier/dsmeta/controllers/SaleController.java
@@ -0,0 +1,26 @@
+package com.sutanrrier.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sutanrrier.dsmeta.entities.Sale;
+import com.sutanrrier.dsmeta.services.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+	@Autowired
+	private SaleService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Sale>> findServices(){
+		return ResponseEntity.status(HttpStatus.OK).body(service.findSales());
+	}
+}
