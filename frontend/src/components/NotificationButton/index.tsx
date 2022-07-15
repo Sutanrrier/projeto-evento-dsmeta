ab07c041c3d58e51346ad98586af3a4623205af7
Sutanrrier <pabloryanbr0@gmail.com>
2022-07-15 14:18:13 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import icon from "../../images/notification-icon.svg";
 import { BASE_URL } from "../../utils/request";
 import "./styles.css";
@@ -8,9 +9,9 @@ type Props = {
 };
 
 function handleClick(id: number) {
-  axios
-    .get(`${BASE_URL}/sales/${id}/notification`)
-    .then((response) => console.log("Sucesso!"));
+  axios.get(`${BASE_URL}/sales/${id}/notification`).then((response) => {
+    toast.info("SMS enviado com sucesso!");
+  });
 }
 
 function NotificationButton({ saleId }: Props) {
